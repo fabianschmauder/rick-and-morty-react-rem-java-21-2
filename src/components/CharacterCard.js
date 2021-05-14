9c7ce84fe8db41734782877a499e069e99c7c632
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-05-14 16:58:55 +0200

    Add styled components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,30 @@
+import styled from "styled-components/macro";
+
+const CardContainer = styled.section`
+  padding: 10px;
+  display: flex;
+
+  align-items: center;
+
+  img {
+    max-width: 50px;
+    border-radius: 20px;
+  }
+
+  h2 {
+    color: black;
+  }
+`;
+
 function CharacterCard({ character }) {
   return (
-    <section>
+    <CardContainer>
       <img src={character.image} alt={character.name} />
-
-      <h2 className={"super-header"}>{character.name}</h2>
-      <div>{character.species}</div>
-    </section>
+      <div>
+        <h2>{character.name}</h2>
+        <div>{character.species}</div>
+      </div>
+    </CardContainer>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
+import styled from "styled-components/macro";
+
+const AppTitle = styled.h1`
+  background-color: aqua;
+  color: deeppink;
+`;
+
+const SuperHeader = styled(AppTitle)`
+  box-shadow: inset 5em 1em gold;
+`;
+
 function Header() {
-  return <h1 className="Header">Rick and Morty Header</h1>;
+  return (
+    <>
+      <AppTitle>Rick and Morty Header</AppTitle>
+      <SuperHeader>Super header</SuperHeader>
+    </>
+  );
 }
 
 export default Header;
